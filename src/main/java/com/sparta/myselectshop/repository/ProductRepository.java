27503dbc77cb672cd1a7b8c64d2ba340ebd28d2a
yawning5 <yawning055@gmail.com>
2025-09-22 05:32:38 +0900

    ProductRepository.findAllByUserAndProductFolderList_FolderId 쿼리문 주석 추가

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -14,4 +14,18 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
 
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
+    // select
+    //p.id,
+    //p.title,
+    //pf.product_id,
+    //pf.folder_id
+    //from product p
+    //left join product_folder pf
+    //on p.id = pf.product_id
+    //where p.user_id = 65
+    //and
+    //pf.folder_id = 3
+    //order by p.id desc
+    //limit 10, 10
+    //;
 }
